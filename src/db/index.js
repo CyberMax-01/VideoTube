4062a29f30bee6bc7416c259e8ea843d4b21f1c8
CyberMax-01 <cybermax2215@gmail.com>
2025-12-13 10:53:15 +0530

    connect mongoDB from src/db/index.js file

---
+++ src/db/index.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose'
+import {DB_NAME} from '../constants.js'
+import 'dotenv/config'
+
+const connectDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(`Connected to database successfully :)\n${connectionInstance.connection.host}`)
+    } catch (error) {
+        console.error("Unable to connect to db !!! \n", error)
+        process.exit(1)
+    }
+}
+
+export default connectDB
--- src/index.js
+++ src/index.js
@@ -1,24 +1,4 @@
-import mongoose from 'mongoose'
 import express from 'express'
-import {DB_NAME} from './constants.js'
-import dotenv from 'dotenv'
+import connectDB from './db/index.js'
 
-dotenv.config()
-const app = express()
-
-;(async () => {
-    try {
-        await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`)
-        app.on('error', (error) => {
-            console.log("Unable to connect to db......\n",error)
-            throw error
-        })
-        console.log("DB connected successfully :)")
-        app.listen(process.env.PORT, () => {
-            console.log(`Listening on port ${process.env.PORT}.....`)
-        })
-    } catch (error) {
-        console.error("Error while connecting db !!!\n", error)
-        throw error
-    }
-})()
+connectDB()
