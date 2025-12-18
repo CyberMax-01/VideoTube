dedbf395d168c40549e3d62e6eeb706d46a71e36
CyberMax-01 <cybermax2215@gmail.com>
2025-12-18 10:08:22 +0530

    all setup complete

--- src/app.js
+++ src/app.js
@@ -12,4 +12,10 @@ app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static('pubic'))
 app.use(cookieParser())
 
+// route import
+import userRouter from './routes/user.routes.js'
+
+// route declaration
+app.use('/api/v1/users', userRouter)
+
 export {app}
